eb98002bd89c734236daa1a975385bd92c2d43a9
tsogiinloovon <mortesla1997@gmail.com>
2021-11-28 23:52:48 +0800

    solved

---
+++ 21_Merge_TwoSortedLists.js
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} list1
+ * @param {ListNode} list2
+ * @return {ListNode}
+ */
+//a 1->2->3
+//b 1->3->4
+
+var mergeTwoLists = function (list1, list2) {
+  const result = { val: 0, next: null };
+  let cur = result;
+  while (list1 && list2) {
+    if (list1.val < list2.val) {
+      cur.next = list1;
+      list1 = list1.next;
+    } else {
+      cur.next = list2;
+      list2 = list2.next;
+    }
+    cur = cur.next;
+  }
+  cur.next = list1 || list2;
+  return result.next;
+};
